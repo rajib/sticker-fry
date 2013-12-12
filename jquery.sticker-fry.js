735dd6e072b59e5dcee8c22b2b04e6201ce794bc
Rajib Chowdhury <chowdhury284@gmail.com>
2013-12-13 02:41:45 +0530

    initial commit.

---
+++ jquery.sticker-fry.js
@@ -0,0 +1,18 @@
+function stickerFry(dom){
+	var regex = new RegExp('\~[^\\s~]+\~', 'g');
+	var target_dom = $('#'+dom);
+	var image_path = "stickers";
+	
+	var result = target_dom.text().match(regex);
+	console.log(result);
+	
+	$.each(result, function( index, value ) {
+		var sticker_image = value.replace(/\~/g, '');
+		var sticker_full_path = image_path + '/' + sticker_image + '.png';
+		var sticker_image_dom = '<img src="'+sticker_full_path+'">';
+		
+		$("#stickerHolder:contains("+value+")").each(function(){
+		    $(this).html($(this).html().replace(value, sticker_image_dom));
+		});	
+	});
+}
